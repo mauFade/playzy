4a40810ce5adb7223ac3c0dcce38ee89bf5376e4
Mauricio Cardoso <mauricio.cds00@gmail.com>
2024-11-20 17:55:19 -0300

    test: improve tests

--- internal/model/user_test.go
+++ internal/model/user_test.go
@@ -10,7 +10,6 @@ import (
 )
 
 func TestNewUserModel(t *testing.T) {
-	// Prepare test data
 	id := uuid.New()
 	name := "John Doe"
 	email := "john.doe@example.com"
@@ -22,10 +21,8 @@ func TestNewUserModel(t *testing.T) {
 	updatedAt := time.Now()
 	createdAt := time.Now()
 
-	// Create a new UserModel using the constructor
 	user := model.NewUserModel(id, name, email, phone, gamertag, password, deleted, deletedAt, updatedAt, createdAt)
 
-	// Validate that the returned user model has the correct values
 	assert.Equal(t, id, user.GetID())
 	assert.Equal(t, name, user.GetName())
 	assert.Equal(t, email, user.GetEmail())
@@ -39,10 +36,9 @@ func TestNewUserModel(t *testing.T) {
 }
 
 func TestSettersAndGetters(t *testing.T) {
-	// Prepare initial user model
+
 	user := &model.UserModel{}
 
-	// Test Setters
 	user.SetName("Jane Doe")
 	user.SetEmail("jane.doe@example.com")
 	user.SetPhone("9876543210")
@@ -50,56 +46,10 @@ func TestSettersAndGetters(t *testing.T) {
 	user.SetPassword("password456")
 	user.SetDeleted(true)
 
-	// Test Getters
 	assert.Equal(t, "Jane Doe", user.GetName())
 	assert.Equal(t, "jane.doe@example.com", user.GetEmail())
 	assert.Equal(t, "9876543210", user.GetPhone())
 	assert.Equal(t, "janedoe", user.GetGamertag())
 	assert.Equal(t, "password456", user.GetPassword())
 	assert.True(t, user.IsDeleted())
-}
-
-func TestDeletedAt(t *testing.T) {
-	// Prepare a non-deleted user with no deletedAt
-	user := &model.UserModel{
-		Deleted:   false,
-		DeletedAt: nil,
-	}
-
-	// Test GetDeletedAt for a non-deleted user
-	assert.Nil(t, user.GetDeletedAt())
-
-	// Set the user as deleted and assign a deletedAt time
-	deletedAt := time.Now()
-	user.SetDeleted(true)
-	user.DeletedAt = &deletedAt
-
-	// Test GetDeletedAt for a deleted user
-	assert.NotNil(t, user.GetDeletedAt())
-	assert.WithinDuration(t, deletedAt, *user.GetDeletedAt(), time.Second)
-}
-
-func TestUpdateTimes(t *testing.T) {
-	// Prepare user model with initial timestamps
-	initialUpdatedAt := time.Now().Add(-time.Hour)
-	initialCreatedAt := time.Now().Add(-2 * time.Hour)
-
-	user := &model.UserModel{
-		UpdatedAt: initialUpdatedAt,
-		CreatedAt: initialCreatedAt,
-	}
-
-	// Test initial values
-	assert.WithinDuration(t, initialUpdatedAt, user.GetUpdatedAt(), time.Second)
-	assert.WithinDuration(t, initialCreatedAt, user.GetCreatedAt(), time.Second)
-
-	// Update user timestamps
-	newUpdatedAt := time.Now()
-	newCreatedAt := time.Now()
-	user.UpdatedAt = newUpdatedAt
-	user.CreatedAt = newCreatedAt
-
-	// Test updated values
-	assert.WithinDuration(t, newUpdatedAt, user.GetUpdatedAt(), time.Second)
-	assert.WithinDuration(t, newCreatedAt, user.GetCreatedAt(), time.Second)
 }
--- internal/repository/user_repository_test.go
+++ internal/repository/user_repository_test.go
@@ -13,186 +13,157 @@ import (
 )
 
 func TestCreate(t *testing.T) {
-	// Criando o mock do banco
+
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("erro ao criar o mock do banco: %v", err)
 	}
 	defer db.Close()
 
-	// Inicializando o repositório com o banco mockado
 	repo := repository.NewUserRepository(db)
 
-	// Preparando os dados de entrada
 	id := uuid.New()
-	user := &model.UserModel{
-		ID:        id,
-		Name:      "John Doe",
-		Email:     "john.doe@example.com",
-		Phone:     "1234567890",
-		Gamertag:  "johnny",
-		Password:  "password123",
-		Deleted:   false,
-		UpdatedAt: time.Now(),
-		CreatedAt: time.Now(),
-	}
+	name := "John Doe"
+	email := "john.doe@example.com"
+	phone := "1234567890"
+	gamertag := "johnny"
+	password := "password123"
+	deleted := false
+	var deletedAt *time.Time = nil
+	updatedAt := time.Now()
+	createdAt := time.Now()
 
-	// Esperando que a query INSERT seja chamada
+	user := model.NewUserModel(id, name, email, phone, gamertag, password, deleted, deletedAt, updatedAt, createdAt)
+
 	mock.ExpectExec("INSERT INTO users").
 		WithArgs(user.GetID(), user.GetName(), user.GetEmail(), user.GetPhone(), user.GetGamertag(), user.GetPassword()).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	// Chamando o método Create
 	err = repo.Create(user)
 
-	// Verificando se a query foi executada corretamente e se o erro é nil
 	assert.NoError(t, err)
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestFindByEmail(t *testing.T) {
-	// Criando o mock do banco
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("erro ao criar o mock do banco: %v", err)
 	}
 	defer db.Close()
 
-	// Inicializando o repositório com o banco mockado
 	repo := repository.NewUserRepository(db)
 
-	// Preparando os dados de entrada
+	id := uuid.New()
+	name := "John Doe"
 	email := "john.doe@example.com"
-	user := &model.UserModel{
-		ID:        uuid.New(),
-		Name:      "John Doe",
-		Email:     email,
-		Phone:     "1234567890",
-		Gamertag:  "johnny",
-		Password:  "password123",
-		Deleted:   false,
-		UpdatedAt: time.Now(),
-		CreatedAt: time.Now(),
-	}
+	phone := "1234567890"
+	gamertag := "johnny"
+	password := "password123"
+	deleted := false
+	var deletedAt *time.Time = nil
+	updatedAt := time.Now()
+	createdAt := time.Now()
 
-	// Esperando que a query SELECT seja chamada
+	user := model.NewUserModel(id, name, email, phone, gamertag, password, deleted, deletedAt, updatedAt, createdAt)
+
 	mock.ExpectQuery("SELECT \\* FROM users WHERE email = ?").
 		WithArgs(email).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "phone", "gamertag", "password", "is_deleted", "deleted_at", "updated_at", "created_at"}).
 			AddRow(user.GetID(), user.GetName(), user.GetEmail(), user.GetPhone(), user.GetGamertag(), user.GetPassword(), user.IsDeleted(), nil, user.GetUpdatedAt(), user.GetCreatedAt()))
 
-	// Chamando o método FindByEmail
 	result, err := repo.FindByEmail(email)
 
-	// Verificando se a query foi executada corretamente e se o resultado é o esperado
 	assert.NoError(t, err)
 	assert.Equal(t, user.GetEmail(), result.GetEmail())
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestFindByPhone(t *testing.T) {
-	// Criando o mock do banco
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("erro ao criar o mock do banco: %v", err)
 	}
 	defer db.Close()
 
-	// Inicializando o repositório com o banco mockado
 	repo := repository.NewUserRepository(db)
 
-	// Preparando os dados de entrada
+	id := uuid.New()
+	name := "John Doe"
+	email := "john.doe@example.com"
 	phone := "1234567890"
-	user := &model.UserModel{
-		ID:        uuid.New(),
-		Name:      "John Doe",
-		Email:     "john.doe@example.com",
-		Phone:     phone,
-		Gamertag:  "johnny",
-		Password:  "password123",
-		Deleted:   false,
-		UpdatedAt: time.Now(),
-		CreatedAt: time.Now(),
-	}
+	gamertag := "johnny"
+	password := "password123"
+	deleted := false
+	var deletedAt *time.Time = nil
+	updatedAt := time.Now()
+	createdAt := time.Now()
 
-	// Esperando que a query SELECT seja chamada
+	user := model.NewUserModel(id, name, email, phone, gamertag, password, deleted, deletedAt, updatedAt, createdAt)
+
 	mock.ExpectQuery("SELECT \\* FROM users WHERE phone = ?").
 		WithArgs(phone).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "phone", "gamertag", "password", "is_deleted", "deleted_at", "updated_at", "created_at"}).
 			AddRow(user.GetID(), user.GetName(), user.GetEmail(), user.GetPhone(), user.GetGamertag(), user.GetPassword(), user.IsDeleted(), nil, user.GetUpdatedAt(), user.GetCreatedAt()))
 
-	// Chamando o método FindByPhone
 	result, err := repo.FindByPhone(phone)
 
-	// Verificando se a query foi executada corretamente e se o resultado é o esperado
 	assert.NoError(t, err)
 	assert.Equal(t, user.GetPhone(), result.GetPhone())
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestFindByGamertag(t *testing.T) {
-	// Criando o mock do banco
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("erro ao criar o mock do banco: %v", err)
 	}
 	defer db.Close()
 
-	// Inicializando o repositório com o banco mockado
 	repo := repository.NewUserRepository(db)
 
-	// Preparando os dados de entrada
+	id := uuid.New()
+	name := "John Doe"
+	email := "john.doe@example.com"
+	phone := "1234567890"
 	gamertag := "johnny"
-	user := &model.UserModel{
-		ID:        uuid.New(),
-		Name:      "John Doe",
-		Email:     "john.doe@example.com",
-		Phone:     "1234567890",
-		Gamertag:  gamertag,
-		Password:  "password123",
-		Deleted:   false,
-		UpdatedAt: time.Now(),
-		CreatedAt: time.Now(),
-	}
+	password := "password123"
+	deleted := false
+	var deletedAt *time.Time = nil
+	updatedAt := time.Now()
+	createdAt := time.Now()
 
-	// Esperando que a query SELECT seja chamada
+	user := model.NewUserModel(id, name, email, phone, gamertag, password, deleted, deletedAt, updatedAt, createdAt)
+
 	mock.ExpectQuery("SELECT \\* FROM users WHERE gamertag = ?").
 		WithArgs(gamertag).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "phone", "gamertag", "password", "is_deleted", "deleted_at", "updated_at", "created_at"}).
 			AddRow(user.GetID(), user.GetName(), user.GetEmail(), user.GetPhone(), user.GetGamertag(), user.GetPassword(), user.IsDeleted(), nil, user.GetUpdatedAt(), user.GetCreatedAt()))
 
-	// Chamando o método FindByGamertag
 	result, err := repo.FindByGamertag(gamertag)
 
-	// Verificando se a query foi executada corretamente e se o resultado é o esperado
 	assert.NoError(t, err)
 	assert.Equal(t, user.GetGamertag(), result.GetGamertag())
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestFindByEmailNotFound(t *testing.T) {
-	// Criando o mock do banco
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("erro ao criar o mock do banco: %v", err)
 	}
 	defer db.Close()
 
-	// Inicializando o repositório com o banco mockado
 	repo := repository.NewUserRepository(db)
 
-	// Preparando o dado de entrada
 	email := "nonexistent@example.com"
 
-	// Esperando que a query SELECT seja chamada e que não retorne nenhum resultado
 	mock.ExpectQuery("SELECT \\* FROM users WHERE email = ?").
 		WithArgs(email).
 		WillReturnError(sql.ErrNoRows)
 
-	// Chamando o método FindByEmail
 	result, err := repo.FindByEmail(email)
 
-	// Verificando se o erro retornado é nil e o resultado também é nil
 	assert.NoError(t, err)
 	assert.Nil(t, result)
 	assert.NoError(t, mock.ExpectationsWereMet())
